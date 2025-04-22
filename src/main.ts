e0ec80e0582e44786cd58e2231ab62cc133c92d7
ikernm <ikernieblamoreno@hotmail.com>
2025-04-22 20:30:35 +0200

    2o edit

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,8 @@
 import "./style.css";
 
+const cartaAleatoria = (): number => {
+    return Math.floor (Math.random() * 12) + 1;
+    if (cartaAleatoria() > 7) {
+        return cartaAleatoria() + 2;
+    }
+}
